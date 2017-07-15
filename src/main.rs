45be07066559d40c075546e7bbd7597746aa71bc
Andrew Prindle <prindle.andrew@gmail.com>
2017-07-14 21:20:14 -0500

    First commit, reads dictionary from cfg file into a HashMap<u8, Vec<String>> and then prints out number of words for each length and a sample last word.

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+
+extern crate argparse;
+extern crate rand;
+extern crate rmp_serde as rmps;
+extern crate serde;
+extern crate time;
+
+use rmps::Deserializer;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::time::Instant;
+
+fn getcfg(file: &str) -> HashMap<u8, Vec<String>> {
+    let mut dict: HashMap<u8, Vec<String>> = HashMap::new();
+    let mut buf = Vec::new();
+    let mut f = File::open(file).expect("Could not open dictionary");
+    f.read_to_end(&mut buf);
+    let mut ds = Deserializer::new(&buf[..]);
+    dict = Deserialize::deserialize(&mut ds).expect("Could not deserialize dictionary data");
+    dict
+}
+
+
+fn main() {
+    let start = Instant::now();
+    let mut dict: HashMap<u8, Vec<String>> = HashMap::new();
+    dict = getcfg("words-sorted.bin");
+    let mut largest = 0u8;
+    let mut lens: HashMap<u8, usize> = HashMap::new();
+    for (key, v) in &dict {
+        if v.len() > 0 {
+            let wrd = match v.last() {
+                Some(w) => w,
+                None => "Cannot find word",
+            };
+            print!("Last word is {}", wrd);
+        }
+        if *key > largest {
+            largest = *key;
+        }
+        lens.insert(*key, v.len());
+        println!(" Length {} has {} words", key, v.len());
+    }
+    
+    
+    let end = start.elapsed();
+    println!("Exec time: {}.{:08}", end.as_secs(), end.subsec_nanos());
+}
