a2f840418014fc1d818b1355e2261af64bc8b9bc
Andrew Prindle <prindle.andrew@gmail.com>
2017-07-20 01:15:43 -0500

    Cleaned up password.rs, removed comments and transform0()

--- src/password.rs
+++ src/password.rs
@@ -66,193 +66,6 @@ fn rand_length(len: u8, min: u8) -> u8 {
     }
 }
 
-/*
-    len     u8      total length of password
-    caps    bool    use capital letters
-    nums    bool    use numbers
-    // minnums u8      min number of numbers
-    punc    bool    use punctuation
-    // minpunc u8      min number of punctuation characters
-    special &str    use special set of punctuation
-*/
-
-pub fn transform0(dict: &HashMap<u8, Vec<String>>, length: u8, caps: bool, mut  nums: bool, leet: bool, mut punc: bool, special: &str) -> String {
-    // get a random number of characters to use for numbers and add_punctuation
-    // check to make sure a word of certain length exists, otherwise get closest length match
-
-    let mut len: u8 = length;
-    
-    // Set a minimum length depending on input arguments
-    if nums && punc && len < 4 {
-        len = 4;
-    } else if nums && len < 3 {
-        len = 3;
-    } else if punc && len < 3 {
-        len = 3;
-    } else if len < 2 {
-        len = 2;
-    }
-    
-    // get a word of length: len-(num+punc)
-    
-    let minword = 2;
-    // maxword not really needed because get_word() automatically gets nearest length
-    // although if there are no words of the length specified by the argument passed
-    // to get_word() then the output password will not be the exact desired length 
-    // let maxword = 23;
-    
-    
-    // Figure out how many characters to use for nums, punc, and a word
-    
-/*    let numlen: u8 = match nums {
-        true => {
-            match len {
-                n if n <= minword => 0,
-                n if n < 5 => 1,
-                n if n < 7 => safe_range(1, 2),
-                n if n < 11 => safe_range(2, 3),
-                n if n >= 11 => safe_range(2, 4),
-                _ => 1,
-            }
-        },
-        false => 0,
-    };*/
-    
-
-
-    
-/*    let punclen: u8 = match punc {
-        true => {
-            match len-numlen {
-                p if p <= minword => 0,
-                p if p < 5 => 1,
-                p if p < 7 => safe_range(1, 2),
-                p if p < 11 => safe_range(2, 3),
-                p if p >= 11 => safe_range(2, 4),
-                _ => 1,
-            }
-        }
-        false => 0,
-    };*/
-    
-    let numlen: u8;
-    let punclen: u8;
-    let has_nums = nums;
-    let has_caps = caps;
-    let has_punc = punc;
-    
-    let mut word_mutated = match leet {
-        true if punc => {
-            punclen = rand_length(len, minword);
-            numlen = 0;
-            len = len-punclen;
-            nums = false;
-            // match leet_speak(get_word(dict, len), true, has_caps, has_nums) {
-            match leet_speak(get_word(dict, len), true) {
-                Some(w) => {
-                    w
-                },
-                _ => mutate_word(get_word(dict, len)),
-            }
-        },
-        true => {
-            punc = false;
-            punclen = 0;
-            numlen = 0;
-            nums = false;
-            // match leet_speak(get_word(dict, len), false, has_caps, has_nums) {
-            match leet_speak(get_word(dict, len), false) {
-                Some(w) => {
-                    w
-                },
-                _ => {
-                    mutate_word(get_word(dict, len))
-                },
-                // clean this all up, the whole Option/Some() stuff
-                // and needing to revert changes to the punc/nums/numlen/punclen
-                // maybe add a gen_num_punc() / gen_num() / gen_punc() functions
-                // idk the whole thing is a mess!!!!! 
-            }
-        },
-        false => {
-            numlen = rand_length(len, minword);
-            punclen = rand_length(len-numlen, minword);
-            len = len-numlen-punclen;
-            // word_mutated = mutate_word(get_word(dict, len));
-            mutate_word(get_word(dict, len))
-        },
-    };
-    println!("transform()\tlen={}\tnumlen={}\tpunclen={}", len, numlen, punclen);
-    
-    // Get a word and start mutating
-    
-    // use the following line instead of the above to test if the 
-    // mutated word exists in the dictionary and do a retry
-    
-    // word_mutated = get_word(dict, len).to_string();
-    
-    if let Some(v) = dict.get(&(word_mutated.len() as u8)) {
-        while v.contains(&word_mutated) {
-            print!("\tMutated word `{}` exists in dictionary, retrying", word_mutated);
-            // word_mutated = mutate_word(get_word(dict, len));
-            let rst = match leet {
-                true if punc => {
-                    // match leet_speak(get_word(dict, len), true, has_caps, has_nums) {
-                    match leet_speak(get_word(dict, len), true) {
-                        Some(w) => w,
-                        _ => {
-                            nums = has_nums;
-                            punc = has_punc;
-                            mutate_word(get_word(dict, len))
-                        },
-                    }
-                },
-                true => {
-                    // match leet_speak(get_word(dict, len), false, has_caps, has_nums) {
-                    match leet_speak(get_word(dict, len), false) {
-                        Some(w) => w,
-                        _ => {
-                            nums = has_nums;
-                            punc = has_punc;
-                            mutate_word(get_word(dict, len))
-                        },
-                    }
-                },
-                false => mutate_word(get_word(dict, len)),
-            };
-            // if rst.is_some() {
-            /*if let Some(w) = rst {
-                word_mutated = w;
-            } else {
-                word_mutated = mutate_word(get_word(dict, len));
-                nums = has_nums;
-                punc = has_punc;
-            }*/
-            println!("\tRetry result: {}", word_mutated);
-        }
-    }
-    
-    debug!("Mutated word: {}", &word_mutated);
-    
-    let word_capitalized: String = if caps{
-        capitalize(&word_mutated)
-    } else {
-        word_mutated
-    };
-    
-    debug!("Capitalized word: {}", word_capitalized);
-    
-    let output = match (nums, punc) {
-        (true, true) => add_numbers(&add_punctuation(&word_capitalized, punclen, special), numlen),
-        (true, false) => add_numbers(&word_capitalized, numlen),
-        (false, false) => word_capitalized,
-        (false, true) => add_punctuation(&word_capitalized, punclen, special),
-    };
-    // info!("Password: {}", output);
-    output
-    
-}
-
 fn is_word(dict: &HashMap<u8, Vec<String>>, word: &str) -> bool {
         if let Some(v) = dict.get(&(word.len() as u8)) {
             if v.contains(&word.to_string()) {
@@ -264,6 +77,15 @@ fn is_word(dict: &HashMap<u8, Vec<String>>, word: &str) -> bool {
             false
         }
 }
+
+/*
+    len     u8      total length of password
+    caps    bool    use capital letters
+    nums    bool    use numbers
+    leet    bool    use leet speak
+    punc    bool    use punctuation
+    special &str    use special set of punctuation
+*/
 
 pub fn transform(dict: &HashMap<u8, Vec<String>>, length: u8, caps: bool, mut  nums: bool, leet: bool, mut punc: bool, special: &str) -> String {
     // leet_speak numbers punctuation 
@@ -302,11 +124,16 @@ pub fn transform(dict: &HashMap<u8, Vec<String>>, length: u8, caps: bool, mut  n
     
     // allocate remaining characters to the word
     len = len-punclen-numlen;
+
+    // get a word
+    // if leet is true call leet_speak() on word
+    // if leet_speak() results in Option::None call transform() with leet=false
+    // if leet == false run mutate_word()
+    // if caps == true run capitalize()
+    // if punclen > 0 run add_punctuation()
+    // if (leet == false || (leet == true && !punc)) && numlen > 0 run add_numbers()
     
     if leet {
-        // let mut step0 = String::new();
-        // new = get_word(dict, len).to_string();
-        
         let mut step0 = get_word(dict, len);
         let rst =  leet_speak(&step0, punc);
         match rst {
@@ -326,26 +153,15 @@ pub fn transform(dict: &HashMap<u8, Vec<String>>, length: u8, caps: bool, mut  n
                     false => step2, // nothing to add
                 };
                 step3
-                /*
-                let step3 = if punc && punclen > 0 {
-                    let step4 = if nums && punc && numlen > 0 {
-                    add_punctuation(&step3)
-                        
-                    } else {
-                        add_punctuation(&step3)
-                    }
-                } else {
-                    step2
-                };*/
-                // let step4 = if nums && punc && numlen > 0 {
-                // } else {
-                // };
             },
             _ => transform(dict, length, caps, nums, false, punc, special),
         }
     } else {
         let step0 = get_word(dict, len);
-        let step1 = mutate_word(step0);
+        let mut step1 = mutate_word(step0);
+        while is_word(dict, &step1) {
+            step1 = mutate_word(get_word(dict, len));
+        }
         let step2 = match caps {
             true => capitalize(&step1),
             false => step1,
@@ -362,15 +178,6 @@ pub fn transform(dict: &HashMap<u8, Vec<String>>, length: u8, caps: bool, mut  n
         };
         step4
     }
-    
-    // get a word
-    // if leet is true call leet_speak() on word
-    // if leet_speak() results in Option::None call transform() with leet=false
-    // if leet == false run mutate_word()
-    // if caps == true run capitalize()
-    // if punclen > 0 run add_punctuation()
-    // if (leet == false || (leet == true && !punc)) && numlen > 0 run add_numbers()
-    
     
     // if leet_speak fails run transform() with leet_speak set to false
     // new
@@ -496,21 +303,6 @@ fn capitalize(word: &str) -> String {
 
 // fn leet_speak(word: &str, adv: bool, caps: bool, nums: bool) -> Option<String > {
 fn leet_speak(word: &str, adv: bool) -> Option<String > {
-    /*
-    let num = safe_range(1, word.len()-1) as u8;
-    
-    // figure out which letters to capitalize
-    let mut v = Vec::new();
-    for _ in 0..num {
-        let mut c = safe_range(0, word.len()/2);
-        while v.contains(&c) {
-            c = safe_range(0, word.len()-1);
-        }
-        v.push(c);
-    }
-    v.sort();
-    */
-    
     // iterate through word, adding positions 
     // of leet letters to a vector
     let mut idxs = Vec::new();
@@ -519,9 +311,6 @@ fn leet_speak(word: &str, adv: bool) -> Option<String > {
             "a" | "e" | "i" | "o" | "s" | "t" | "z" => idxs.push(i),
             _ => {}
         }
-        /* if "aeiostz".to_string().contains(word[i..i+1]) {
-            idxs.push(i);
-        }*/
     }
     // shuffle vector
     let mut rg = thread_rng();
@@ -572,23 +361,6 @@ fn leet_speak(word: &str, adv: bool) -> Option<String > {
         // mutate_word(word)
         None
     }
-    
-    /*
-    let mut new = String::with_capacity(word.len());
-    
-    for l in 0..word.len() {
-        if v.contains(&l) {
-            // new.push_str(&word[l..l+1].to_uppercase());
-            
-        } else {
-            // new.push_str(&word[l..l+1]);
-            
-        }
-    }
-    
-    debug!("leet_speak():\n\tOriginal = {}\n\tLeet = {}", word, new);
-    new
-    */
 }
 
 // random_insert() is used to insert a character/string at a
